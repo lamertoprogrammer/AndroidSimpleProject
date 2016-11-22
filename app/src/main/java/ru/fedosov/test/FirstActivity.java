d6db4e4ffd10a6382dda692e5d020b4b60aaa6b5
Maxim <femax339@gmail.com>
2016-11-22 13:59:16 +0300

    stage

--- app/src/main/java/ru/fedosov/test/FirstActivity.java
+++ app/src/main/java/ru/fedosov/test/FirstActivity.java
@@ -1,11 +1,14 @@
 package ru.fedosov.test;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class FirstActivity extends AppCompatActivity {
@@ -15,65 +18,70 @@ public class FirstActivity extends AppCompatActivity {
 
     private EditText mEditText;
     private Toolbar mToolbar;
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mEditText = (EditText) findViewById(R.id.edittext);
-        Log.i(TAG,"onCreate");
+        Log.i(TAG, "onCreate");
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), SecondActivity.class));
+            }
+        });
     }
 
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG,"onCreate");
+        Log.i(TAG, "onCreate");
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG,"onResume");
+        Log.i(TAG, "onResume");
 
     }
 
     @Override
     protected void onPause() {
-        Log.i(TAG,"onPause");
+        Log.i(TAG, "onPause");
         super.onPause();
     }
 
     @Override
     protected void onStop() {
-        Log.i(TAG,"onStop");
+        Log.i(TAG, "onStop");
         super.onStop();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.i(TAG,"onRestart");
+        Log.i(TAG, "onRestart");
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        Log.i(TAG,"onRestart");
+        Log.i(TAG, "onRestart");
         restoreActionBar();
         return true;
     }
 
     protected void restoreActionBar() {
         ActionBar actionBar = getSupportActionBar();
-        if (actionBar == null) return;
-        actionBar.setDisplayShowHomeEnabled(true);
-        actionBar.setHomeButtonEnabled(true);
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        if(actionBar!=null)
         actionBar.setTitle(R.string.app_name);
     }
 }
--- app/src/main/java/ru/fedosov/test/SecondActivity.java
+++ app/src/main/java/ru/fedosov/test/SecondActivity.java
@@ -1,10 +1,52 @@
 package ru.fedosov.test;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by fedosov on 11/22/16.
  */
 
 public class SecondActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        mToolbar = (Toolbar) findViewById(R.id.moretoolbar);
+        setSupportActionBar(mToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        restoreActionBar();
+        return true;
+    }
+
+    protected void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar == null) return;
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(R.string.app_name);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
